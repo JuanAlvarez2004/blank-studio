c35d3bd4f367c709db0b5f55e9aa081a705ce016
JuanAlvarez2004 <168690900+JuanAlvarez2004@users.noreply.github.com>
2025-10-17 17:03:57 -0500

    feat: add Lenis for smooth scrolling and update layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,22 +5,39 @@ import Home from "../../sections/Home";
 import About from "../../sections/About";
 import Services from "../../sections/Services";
 import Contact from "../../sections/Contact";
+import { ReactLenis } from 'lenis/react'
+import { useEffect } from "react";
+import gsap from "gsap";
 
 export default function Layout() {
   const ballCursorRef = useRef(null)
+  const lenisRef = useRef()
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+
+    gsap.ticker.add(update)
+
+    return () => gsap.ticker.remove(update)
+  }, [])
 
   return (
-    <div className="font-family-lato min-h-dvh min-w-dvw flex flex-col overflow-x-hidden">
-      <Header />
+    <>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <div className="font-family-lato min-h-dvh min-w-dvw flex flex-col overflow-x-hidden">
+        <Header />
 
-      <main className="flex-grow">
-        <Home />
-        <About />
-        <Services />
-        <Contact />
-      </main>
+        <main className="flex-grow">
+          <Home />
+          <About />
+          <Services />
+          <Contact />
+        </main>
 
-      <BallCursor ref={ballCursorRef} />
-    </div>
+        <BallCursor ref={ballCursorRef} />
+      </div>
+    </>
   );
 }
